fc26e4c318427b2a2ac05029cea8f96afed304d9
Japi20002 <omeragam04@gmail.com>
2024-10-24 15:51:54 +0300

    04-13 weird destructor

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    charged: bool,
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb{charged: true}
+    }
+
+    pub fn defuse(&mut self) {
+        self.charged = false;
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if self.charged {
+            panic!("Bomb wasn't defused!");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
